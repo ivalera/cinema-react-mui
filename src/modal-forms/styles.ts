db42db7608b8a4a0b146fe79916933c602d53645
Valera Savchenko <ivalera.develop@gmail.com>
2024-07-17 22:23:17 +0300

    Changed modal-form width.

--- src/modal-forms/styles.ts
+++ src/modal-forms/styles.ts
@@ -3,7 +3,7 @@ const MODAL_STYLE = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: '340px',
     bgcolor: 'background.paper',
     borderRadius: 1,
     boxShadow: 24,
